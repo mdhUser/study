e90ae4e8a91da6d8b2ffbce32cf9930dec69167f
mdhUser <734570993@qq.com>
2022-04-15 22:02:46 +0800

    一些算法 2022/3/27

---
+++ leetcode/src/main/java/org/maxwell/algorithm/Log.java
@@ -0,0 +1,22 @@
+package org.maxwell.algorithm;
+
+/**
+ * @description:
+ * @author: Maxwell
+ * @email: maodihui@foxmail.com
+ * @date: 2022/4/15 21:55
+ */
+public class Log {
+
+
+
+
+    public static void pow(int n){
+
+        Math.log()
+
+    }
+
+
+
+}
