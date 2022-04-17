fd131dad00f46a39ba9dac4c670dbde7b5aaeaaf
mdhUser <734570993@qq.com>
2022-04-17 22:51:15 +0800

    一些算法 2022/4/17

--- leetcode/src/main/java/org/maxwell/algorithm/Log.java
+++ leetcode/src/main/java/org/maxwell/algorithm/Log.java
@@ -9,14 +9,17 @@ package org.maxwell.algorithm;
 public class Log {
 
 
-
-
-    public static void pow(int n){
-
-        Math.log()
+    public static void main(String[] args) {
 
     }
 
+    public static boolean pow(int n) {
+
+        if (n <= 0) return false;
+        while (n % 2 == 0) n /= 2;
+        return n == 1;
+
+    }
 
 
 }
---
+++ leetcode/src/main/java/org/maxwell/algorithm/Solution819.java
@@ -0,0 +1,93 @@
+package org.maxwell.algorithm;
+
+import java.util.*;
+import java.util.function.Function;
+
+/**
+ * @description:
+ * @author: Maxwell
+ * @email: maodihui@foxmail.com
+ * @date: 2022/4/17 17:49
+ */
+public class Solution819 {
+
+
+    public static void main(String[] args) {
+
+        System.out.println(mostCommonWord("a, a, a, a, b,b,b,c, c", new String[]{"a"}));
+
+    }
+
+
+    public static boolean isPowerOfTwo(int n) {
+
+        if (n == 1) return true;
+        int k = 2;
+        for (int i = 1; i <= 31; i++) {
+            k *= 2;
+            if (k == Math.abs(n)) {
+                return true;
+            }
+            if (k > Math.abs(n)) {
+                return false;
+            }
+        }
+
+        return false;
+    }
+
+
+    public static String mostCommonWord(String paragraph, String[] banned) {
+
+        String str = paragraph.toLowerCase();
+        Map<String, Integer> map = new TreeMap<>();
+        Comparator<Map.Entry<String, Integer>> comparator = (o1, o2) -> o2.getValue() - o1.getValue();
+
+        String[] strs = str.split(" ");
+        for (String s : strs) {
+            char c = s.charAt(s.length() - 1);
+            String words;
+            if (c > 'z' || c < 'a') {
+                words = s.substring(0, s.length() - 1);
+            } else {
+                words = s;
+            }
+
+            String[] split = words.split(",");
+            if (split.length > 1) {
+                for (String s1 : split) {
+                    getTreeMap(banned,map,s1);
+                }
+            }else {
+                getTreeMap(banned, map, words);
+            }
+        }
+
+        List<Map.Entry<String, Integer>> list = new ArrayList<>(map.entrySet());
+        Collections.sort(list, comparator);
+
+        return list.get(0).getKey();
+
+    }
+
+    private static Map<String,Integer> getTreeMap(String[] banned, Map<String, Integer> map, String words) {
+
+        Function<String[], Boolean> ass = a -> {
+            for (String s1 : a) {
+                if (s1.equals(words))
+                    return false;
+            }
+            return true;
+        };
+
+        if (ass.apply(banned)) {
+            if (map.containsKey(words)) {
+                map.put(words, map.get(words) + 1);
+            } else {
+                map.put(words, 1);
+            }
+        }
+        return map;
+    }
+
+}
