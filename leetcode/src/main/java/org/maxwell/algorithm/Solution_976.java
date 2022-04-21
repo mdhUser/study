d6c55e4077e3e340ae0e6225e62b5996d0a86679
mdhUser <734570993@qq.com>
2022-04-21 19:59:26 +0800

    一些算法 2022/4/21

---
+++ leetcode/src/main/java/org/maxwell/algorithm/Solution_976.java
@@ -0,0 +1,29 @@
+package org.maxwell.algorithm;
+
+import java.util.Arrays;
+
+/**
+ * @description: 三角形的最大周长
+ * @author: Maxwell
+ * @email: maodihui@foxmail.com
+ * @date: 2022/4/21 16:14
+ */
+public class Solution_976 {
+
+
+    public static void main(String[] args) {
+
+        System.out.println(largestPerimeter(new int[]{1, 2, 1}));
+    }
+
+
+    public static int largestPerimeter(int[] nums) {
+        Arrays.sort(nums);
+        int len = nums.length - 1;
+        for (int i = len; i >= 2; --i)
+            if (nums[i - 1] + nums[i - 2] > nums[i]) return nums[i] + nums[i - 1] + nums[i - 2];
+        return 0;
+    }
+
+
+}
---
+++ leetcode/src/main/java/org/maxwell/algorithm/Solution_add.java
@@ -0,0 +1,42 @@
+package org.maxwell.algorithm;
+
+/**
+ * @description: 不用+号的+法
+ * @author: Maxwell
+ * @email: maodihui@foxmail.com
+ * @date: 2022/4/16 16:20
+ */
+public class Solution_add {
+
+    public static void main(String[] args) {
+        System.out.println(getSum(5, 6));
+        System.out.println(divide(-2147483648,
+                -1));
+    }
+
+    public int mySqrt(int x) {
+        double sqrt = Math.sqrt(x);
+        return (int) sqrt;
+    }
+
+    public static int getSum(int a, int b) {
+
+        while (b != 0) {
+            int add = (a & b) << 1;
+            a = a ^ b;
+            b = add;
+        }
+        return a;
+    }
+
+    /**
+     *  一般的除法
+     * @param dividend
+     * @param divisor
+     * @return
+     */
+    public static int divide(int dividend, int divisor) {
+        return (int) Math.ceil(dividend / divisor);
+    }
+
+}
