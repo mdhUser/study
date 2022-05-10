8b895ec92b63f7bdbda0078691d4c543a0671187
mdhUser <734570993@qq.com>
2022-05-10 22:31:58 +0800

    数组练习 III

---
+++ jvm/src/main/java/org/maxwell/jvm/HelloWorld.java
@@ -0,0 +1,15 @@
+package org.maxwell.jvm;
+
+/**
+ * @description:
+ * @author: Maxwell
+ * @email: maodihui@foxmail.com
+ * @date: 2022/5/9 0:43
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("hello world !!!");
+    }
+
+}
---
+++ leetcode/src/main/java/org/maxwell/algorithm/矩阵/Solution_36_有效数独.java
@@ -0,0 +1,39 @@
+package org.maxwell.algorithm.矩阵;
+
+/**
+ * @description:
+ * @author: Maxwell
+ * @email: maodihui@foxmail.com
+ * @date: 2022/5/10 21:37
+ */
+public class Solution_36_有效数独 {
+
+
+    /**
+     * 官方题解
+     *
+     * @param board
+     * @return
+     */
+    public boolean isValidSudoku(char[][] board) {
+
+        int[][] rows = new int[9][9];
+        int[][] columns = new int[9][9];
+        int[][][] subboxes = new int[3][3][9];
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char c = board[i][j];
+                if (c != '.') {
+                    int index = c - '0' - 1;
+                    rows[i][index]++; //行计数
+                    columns[j][index]++; //列计数
+                    subboxes[i / 3][j / 3][index]++; //子框计数
+                    if (rows[i][index] > 1 || columns[j][index] > 1 || subboxes[i / 3][j / 3][index] > 1) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
