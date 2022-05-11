f3e774aab2cecdc08f0fe83dcd5fec907ed873dc
mdhUser <734570993@qq.com>
2022-05-11 23:45:05 +0800

    链表1

---
+++ leetcode/src/main/java/org/maxwell/algorithm/easy/Solution_21_合并两个有序链表.java
@@ -0,0 +1,79 @@
+package org.maxwell.algorithm.easy;
+
+/**
+ * @description: 升序
+ * @author: Maxwell
+ * @email: maodihui@foxmail.com
+ * @date: 2022/5/11 14:18
+ */
+public class Solution_21_合并两个有序链表 {
+
+
+    /**
+     * 递归
+     *
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null)
+            return l2;
+        else if (l2 == null)
+            return l1;
+        else if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l2.next, l1);
+            return l2;
+        }
+    }
+
+
+    /**
+     * 迭代法
+     *
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public ListNode mergeTwoLists_iterate(ListNode l1, ListNode l2) {
+        ListNode prehead = new ListNode(-1);
+        ListNode prev = prehead;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                prev.next = l1;
+                l1 = l1.next;
+            } else {
+                prev.next = l2;
+                l2 = l2.next;
+            }
+            prev = prev.next;
+        }
+
+        // 合并后 l1 和 l2 最多只有一个还未被合并完，我们直接将链表末尾指向未合并完的链表即可
+        prev.next = l1 == null ? l2 : l1;
+        return prehead.next;
+    }
+
+
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+}
---
+++ leetcode/src/main/java/org/maxwell/algorithm/链表/Solution_141_环形链表.java
@@ -0,0 +1,63 @@
+package org.maxwell.algorithm.链表;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @description:
+ * @author: Maxwell
+ * @email: maodihui@foxmail.com
+ * @date: 2022/5/11 12:22
+ */
+public class Solution_141_环形链表 {
+    /**
+     * hashset解决
+     *
+     * @param head
+     * @return
+     */
+    public boolean hasCycle(ListNode head) {
+        Set<ListNode> history = new HashSet<>();
+        while (head != null) {
+            if (!history.add(head)) {
+                return true;
+            }
+            head = head.next;
+        }
+        return false;
+    }
+
+    /**
+     * 快慢指针 时间复杂度O(n) 空间复杂度O(1)
+     *
+     * @param head
+     * @return
+     */
+    public boolean hasCycle_pointer(ListNode head) {
+        if (head == null || head.next == null)
+            return false;
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (fast != slow) {
+            if (fast == null || fast.next == null)
+                return false;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return true;
+    }
+
+
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
