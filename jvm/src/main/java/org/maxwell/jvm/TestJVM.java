6598fe0e81c86011599dc1d59134898fd323d07e
Maxwell <maodihui@foxmail.com>
2022-09-12 15:18:43 +0800

    JVM test

---
+++ jvm/src/main/java/org/maxwell/jvm/TestJVM.java
@@ -0,0 +1,21 @@
+package org.maxwell.jvm;
+
+/**
+ * @description:
+ * @author: maxwell
+ * @email: maodihui@foxmail.com
+ * @date: 2022/9/10 22:55
+ */
+public class TestJVM {
+
+    public static void main(String[] args) {
+
+        byte[] array = new byte[1024 * 1024];
+        array = new byte[1024 * 1024];
+        array = new byte[1024 * 1024];
+        array = null;
+        byte[] array1 = new byte[2*1024 * 1024];
+
+    }
+
+}
